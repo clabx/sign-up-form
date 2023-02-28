87e05d6d3fba46c4a95e2c84828d153a5e029881
mcal-mc <dev@mcalvo.ar>
2023-02-27 21:32:35 -0300

    Improve validation with js
    Now every field will be checked in real time, to see if the user filled it correctly and warn correspondly.

--- build/js/main.js
+++ build/js/main.js
@@ -2,9 +2,17 @@ const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirmPassword');
 const invalidConfirmPassword = document.querySelector('#invalidConfirmedPassword');
 const invalidPassword = document.querySelector('#invalidPassword');
+
+const email = document.querySelector('#email');
 const invalidEmail = document.querySelector('#invalidEmail');
+
+const lastName = document.querySelector('#lastName')
 const invalidLastName = document.querySelector('#invalidLastName');
+
+const firstName = document.querySelector('firstName')
 const invalidName = document.querySelector('#invalidName');
+
+const formInput = document.querySelectorAll('input')
 
 function checkPassword(){
             if(password.value != confirmPassword.value){
@@ -19,11 +27,20 @@ function checkPassword(){
         }
 }
 
-
-
 confirmPassword.addEventListener('input', () => {
     password.addEventListener('input', () => {
         checkPassword();
     })
         checkPassword();
 })
+
+formInput.forEach((form) => {form.addEventListener('input', () => {
+    if(form.validity.valid == false){
+        if(form.validity.valueMissing){
+            form.nextElementSibling.textContent = "Required.";
+        } else{
+            form.nextElementSibling.textContent = "Incorrect form.";
+        }
+    } 
+})})
+
